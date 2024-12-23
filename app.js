54f1cb8ed2e0b52266f2055aac92ca88993a9ef0
adarsh rakshit <adarshrakshit07@gmail.com>
2024-12-24 03:12:35 +0530

    done

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const express = require('express')
-const app = express()
-const http = require('http')
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require("path");
+const socketio = require("socket.io");
 
-const socketio = require("socket.io")
+const server = http.createServer(app);
 
-const server = http.createServer(app)
+const io = socketio(server);
 
-const io = socketio(server)
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection", function (socket) {
+    socket.on("send-location",(data)=>{
+        io.emit("recieve-location",{id:socket.id, ...data});
+    })
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnected",socket.id);
+    })
+});
 
 
 app.get("/", (req,res)=>{
-    res.send("hey");
+    res.render("index");
 })
 
 server.listen(3000);
